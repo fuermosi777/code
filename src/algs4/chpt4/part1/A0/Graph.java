b6b6e8acbe6bbb24c3c5b6b7f5565b55b5e56841
fuermosi777 <liuhao1990@gmail.com>
2016-08-16 16:52:59 -0700

    cp4

---
+++ src/algs4/chpt4/part1/A0/Graph.java
@@ -0,0 +1,37 @@
+package algs4.chpt4.part1.A0;
+
+import edu.princeton.cs.algs4.Bag;
+
+/**
+ * Created by hao on 8/16/16.
+ */
+public class Graph {
+
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+
+}
