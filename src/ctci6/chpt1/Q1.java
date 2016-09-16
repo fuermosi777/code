6c1f5a3e76bac14dfcccc7d313b5edc13c80993e
fuermosi777 <liuhao1990@gmail.com>
2016-09-16 13:35:07 -0700

    q1

---
+++ src/ctci6/chpt1/Q1.java
@@ -0,0 +1,26 @@
+package ctci6.chpt1;
+
+import java.util.Arrays;
+
+/**
+ * Created by hao on 9/16/16.
+ */
+public class Q1 {
+
+    public static boolean isUnique(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        assert isUnique("wtf");
+        assert (isUnique("wwf") == true);
+        System.out.println(isUnique("wwf"));
+    }
+}
