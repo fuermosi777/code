3464dbbc941049705228d27b959209d83a2f017c
Hao Liu <liuhao1990@gmail.com>
2016-10-18 17:32:44 -0700

    update

---
+++ src/ctci6/chpt8/Q13.java
@@ -0,0 +1,13 @@
+package ctci6.chpt8;
+
+/**
+ * Created by hao on 10/19/16.
+ */
+public class Q13 {
+    public class BoxGraph {
+
+    }
+    public class BoxGraphEdge {
+        public int weight;
+    }
+}
