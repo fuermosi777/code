712388aa1cd44f81e565bf101e99f37513a04df9
fuermosi777 <liuhao1990@gmail.com>
2016-10-23 12:08:26 -0700

    update 8.13

--- src/ctci6/chpt8/Q13.java
+++ src/ctci6/chpt8/Q13.java
@@ -4,10 +4,12 @@ package ctci6.chpt8;
  * Created by hao on 10/19/16.
  */
 public class Q13 {
-    public class BoxGraph {
-
-    }
-    public class BoxGraphEdge {
-        public int weight;
+    public static void main(String[] args) {
+        int A = 3;
+        int K = 5;
+        int P = 1;
+//        System.out.println((double)P / (double)(K - A));
+        System.out.println((int) Math.floor((double)P / (double)(K - A)));
+        System.out.println((int) Math.ceil(0.5));
     }
 }
