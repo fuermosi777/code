450815383470c34c78e42cc0a6d75f65030836f7
Hao Liu <haol@yahoo-inc.com>
2017-06-26 15:01:15 -0700

    Q25

--- src/leetcode/java/25.reverse-nodes-in-k-group.java
+++ src/leetcode/java/25.reverse-nodes-in-k-group.java
@@ -36,16 +36,49 @@
  * For k = 3, you should return: 3->2->1->4->5
  * 
  */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
 public class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        
+        if (head == null || head.next == null) return head;
+        if (k == 1) return head;
+        int size = 0;
+        ListNode n = head;
+        ListNode newHead = null;
+        while (n != null) {
+            size++;
+            if (size == k) newHead = n;
+            n = n.next;
+        }
+        if (k > size) return head;
+
+        int i = 1; // counter
+        n = head; // reset the flag
+        ListNode lt = null, // last partial tail
+            next = null, // next node
+            t = null, // current tail
+            f = null; // former node
+        while (i <= size - size % k) {
+            if ((i - 1) % k == 0) { // is partial tail
+                f = null;
+                t = n;
+            }
+            if (i % k == 0) { // is partial head
+                if (lt != null) {
+                    lt.next = n;
+                }
+                lt = t;
+            }
+            next = n.next;
+            if (f != null) {
+                n.next = f;
+            }
+
+            f = n;
+            n = next;
+            i++;
+        }
+        lt.next = n;
+
+        return newHead;
     }
 }
