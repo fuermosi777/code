49ab4bd52bce91fa8b43fea6e7b2a2f854b7ce44
Hao Liu <haol@yahoo-inc.com>
2017-06-27 15:36:39 -0700

    26

--- src/leetcode/java/26.remove-duplicates-from-sorted-array.java
+++ src/leetcode/java/26.remove-duplicates-from-sorted-array.java
@@ -1,34 +1,32 @@
-/*
- * [26] Remove Duplicates from Sorted Array
- *
- * https://leetcode.com/problems/remove-duplicates-from-sorted-array
- *
- * Easy (35.56%)
- * Total Accepted:    
- * Total Submissions: 
- * Testcase Example:  '[]'
- *
- * 
- * Given a sorted array, remove the duplicates in place such that each element
- * appear only once and return the new length.
- * 
- * 
- * Do not allocate extra space for another array, you must do this in place
- * with constant memory.
- * 
- * 
- * 
- * For example,
- * Given input array nums = [1,1,2],
- * 
- * 
- * Your function should return length = 2, with the first two elements of nums
- * being 1 and 2 respectively. It doesn't matter what you leave beyond the new
- * length.
- * 
- */
+import java.util.*;
+
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        
+        if (nums.length == 0 || nums.length == 1) return nums.length;
+        int i = 1, j = 1;
+        int d = nums[0]; // duplicate
+        while (i < nums.length && j < nums.length) {
+            if (nums[i] <= nums[i - 1]) {
+                if (j <= i) j = i + 1;
+                if (nums[i] > d) d = nums[i];
+                while (j < nums.length && nums[j] <= d) {
+                    j++;
+                }
+                if (j < nums.length) {
+                    d = nums[j];
+                    swap(nums, i, j);
+                } else {
+                    break;
+                }
+            }
+            i++;
+        }
+
+        return i;
+    }
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
