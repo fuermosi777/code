71a341321f46349961a6d7d759859e23d57acfad
fuermosi777 <liuhao1990@gmail.com>
2017-06-28 00:04:58 -0700

    WIP 27

---
+++ src/leetcode/javascript/28.implement-strstr.js
@@ -0,0 +1,26 @@
+/*
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr
+ *
+ * Easy (27.79%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '""\n""'
+ *
+ * 
+ * Implement strStr().
+ * 
+ * 
+ * Returns the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+};
