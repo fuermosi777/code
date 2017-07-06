b6db1ceb79268aecc3c4a93da986601864e78d7b
Hao Liu <haol@yahoo-inc.com>
2017-07-06 11:39:40 -0700

    Q31

--- leetcode/javascript/31.next-permutation.js
+++ leetcode/javascript/31.next-permutation.js
@@ -1,32 +1,3 @@
-/*
- * [31] Next Permutation
- *
- * https://leetcode.com/problems/next-permutation
- *
- * Medium (28.66%)
- * Total Accepted:    
- * Total Submissions: 
- * Testcase Example:  '[1]'
- *
- * 
- * Implement next permutation, which rearranges numbers into the
- * lexicographically next greater permutation of numbers.
- * 
- * 
- * If such arrangement is not possible, it must rearrange it as the lowest
- * possible order (ie, sorted in ascending order).
- *  * 
- * The replacement must be in-place, do not allocate extra memory.
- * 
- * 
- * Here are some examples. Inputs are in the left-hand column and its
- * corresponding outputs are in the right-hand column.
- * 1,2,3 → 1,3,2
- * 3,2,1 → 1,2,3
- * 1,1,5 → 1,5,1
- * 
- */
-
 /**
  * Iterate backward, found ascending a[i], a[i+1] and look for the min (larger than a[i]) after it, swap the i with the min and sort i ... n
  * For sorting, use in-place heap sort
@@ -39,10 +10,10 @@ function swap(nums, i, j) {
 }
 
 function min(nums, t, lo, hi) {
-  let mi = hi;
-  let cm = nums[hi];
+  let mi = lo;
+  let cm = nums[lo];
   for (let i = lo; i <= hi; i++) {
-    if (nums[i] > nums[t] && nums[i] < cm) {
+    if (nums[i] > nums[t] && nums[i] <= cm) {
       mi = i;
       cm = nums[i];
     }
@@ -73,6 +44,6 @@ var nextPermutation = function(nums) {
   reverse(nums, 0, nums.length - 1);
 };
 
-let arr = [1, 2, 4, 3, 2];
-nextPermutation(arr);
-console.log(arr);
+// let arr = [2,3,1,3,3];
+// nextPermutation(arr);
+// console.log(arr);
