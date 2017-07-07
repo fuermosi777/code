8903d499d7153aa5105d10570efbcd913055feab
Hao Liu <haol@yahoo-inc.com>
2017-07-07 12:20:25 -0700

    Q34

--- leetcode/javascript/34.search-for-a-range.js
+++ leetcode/javascript/34.search-for-a-range.js
@@ -1,31 +1,49 @@
-/*
- * [34] Search for a Range
- *
- * https://leetcode.com/problems/search-for-a-range
- *
- * Medium (31.27%)
- * Total Accepted:    
- * Total Submissions: 
- * Testcase Example:  '[5,7,7,8,8,10]\n8'
- *
- * Given an array of integers sorted in ascending order, find the starting and
- * ending position of a given target value.
- * 
- * Your algorithm's runtime complexity must be in the order of O(log n).
- * 
- * If the target is not found in the array, return [-1, -1].
- * 
- * 
- * For example,
- * Given [5, 7, 7, 8, 8, 10] and target value 8,
- * return [3, 4].
- * 
- */
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    
+  let a = -1, b = -1;
+
+  let lo = 0, hi = nums.length - 1;
+  while (lo <= hi) {
+    let mid = lo + Math.floor((hi - lo) / 2);
+    if (nums[mid] === target) {
+      if (mid === 0) {
+        a = mid; break;
+      } else if (nums[mid - 1] < target) {
+        a = mid; break;
+      } else {
+        hi = mid - 1;
+      }
+    } else if (nums[mid] < target) {
+      lo = mid + 1;
+    } else {
+      hi = mid - 1;
+    }
+  }
+
+  lo = 0; 
+  hi = nums.length - 1;
+  while (lo <= hi) {
+    let mid = lo + Math.floor((hi - lo) / 2);
+    if (nums[mid] === target) {
+      if (mid === nums.length - 1) {
+        b = mid; break;
+      } else if (nums[mid + 1] > target) {
+        b = mid; break;
+      } else {
+        lo = mid + 1;
+      }
+    } else if (nums[mid] < target) {
+      lo = mid + 1;
+    } else {
+      hi = mid - 1;
+    }
+  }
+
+  return [a, b];
 };
+
+// console.log(searchRange([], 5));
