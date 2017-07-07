7e04bc6028db37ef501fcbc274e65340386c2472
Hao Liu <haol@yahoo-inc.com>
2017-07-07 16:39:18 -0700

    Q36

---
+++ leetcode/javascript/36.valid-sudoku.js
@@ -0,0 +1,68 @@
+/*
+ * [36] Valid Sudoku
+ *
+ * https://leetcode.com/problems/valid-sudoku
+ *
+ * Medium (35.36%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '[".87654321","2........","3........","4........","5........","6........","7........","8........","9........"]'
+ *
+ * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+ * 
+ * The Sudoku board could be partially filled, where empty cells are filled
+ * with the character '.'.
+ * 
+ * 
+ * 
+ * A partially filled sudoku which is valid.
+ * 
+ * 
+ * Note:
+ * A valid Sudoku board (partially filled) is not necessarily solvable. Only
+ * the filled cells need to be validated.
+ * 
+ */
+function validate(nums) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map.hasOwnProperty(nums[i]) && nums[i] !== '.') return false;
+    map[nums[i]] = true;
+  }
+  return true;
+}
+
+function add9(board, i, j) {
+  let nums = "";
+  for (let a = i; a < i + 3; a++) {
+    for (let b = j; b < j + 3; b++) {
+      nums += board[a][b];
+    }
+  }
+  return nums;
+}
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+  for (let i = 0; i < board.length; i++) {
+    if (!validate(board[i])) return false;
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    let nums = "";
+    for (let j = 0; j < board.length; j++) {
+      nums += board[j][i];
+    }
+    if (!validate(nums)) return false;
+  }
+
+  let start = [0, 3, 6];
+  for (let i = 0; i < start.length; i++) {
+    for (let j = 0; j < start.length; j++) {
+      if (!validate(add9(board, start[i], start[j]))) return false;
+    }
+  }
+  return true;
+};
