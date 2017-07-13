2cf3804ed9aaeaa6529dbab40f85328f0d293299
Hao Liu <haol@yahoo-inc.com>
2017-07-13 16:08:26 -0700

    rm unused desc

--- leetcode/javascript/47.permutations-ii.js
+++ leetcode/javascript/47.permutations-ii.js
@@ -1,30 +1,3 @@
-/*
- * [47] Permutations II
- *
- * https://leetcode.com/problems/permutations-ii
- *
- * Medium (32.43%)
- * Total Accepted:    
- * Total Submissions: 
- * Testcase Example:  '[1,1,2]'
- *
- * 
- * Given a collection of numbers that might contain duplicates, return all
- * possible unique permutations.
- * 
- * 
- * 
- * For example,
- * [1,1,2] have the following unique permutations:
- * 
- * [
- * ⁠ [1,1,2],
- * ⁠ [1,2,1],
- * ⁠ [2,1,1]
- * ]
- * 
- * 
- */
 function insert(nums, index, a) {
   let res = [];
   let flag = false;
---
+++ leetcode/javascript/48.rotate-image.js
@@ -0,0 +1,22 @@
+/*
+ * [48] Rotate Image
+ *
+ * https://leetcode.com/problems/rotate-image
+ *
+ * Medium (38.31%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '[[1]]'
+ *
+ * You are given an n x n 2D matrix representing an image.
+ * Rotate the image by 90 degrees (clockwise).
+ * Follow up:
+ * Could you do this in-place?
+ */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    
+};
