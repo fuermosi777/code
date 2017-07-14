31f15f870f373c486c5fe74053a0184f9051564e
Hao Liu <haol@yahoo-inc.com>
2017-07-14 15:05:56 -0700

    Q96

--- leetcode/javascript/48.rotate-image.js
+++ leetcode/javascript/48.rotate-image.js
@@ -1,19 +1,3 @@
-/*
- * [48] Rotate Image
- *
- * https://leetcode.com/problems/rotate-image
- *
- * Medium (38.31%)
- * Total Accepted:    
- * Total Submissions: 
- * Testcase Example:  '[[1]]'
- *
- * You are given an n x n 2D matrix representing an image.
- * Rotate the image by 90 degrees (clockwise).
- * Follow up:
- * Could you do this in-place?
- */
-
 function swap(matrix, row, col) {
   let n = matrix.length;
   let iRow = col;
