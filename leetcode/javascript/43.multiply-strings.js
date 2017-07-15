3bf5cde20d5ac34ac416bdf2e812df32f94bf7fa
fuermosi777 <liuhao1990@gmail.com>
2017-07-15 11:47:33 -0700

    43?

---
+++ leetcode/javascript/43.multiply-strings.js
@@ -0,0 +1,31 @@
+/*
+ * [43] Multiply Strings
+ *
+ * https://leetcode.com/problems/multiply-strings
+ *
+ * Medium (26.89%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '"0"\n"0"'
+ *
+ * Given two non-negative integers num1 and num2 represented as strings, return
+ * the product of num1 and num2.
+ * 
+ * Note:
+ * 
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contains only digits 0-9.
+ * Both num1 and num2 does not contain any leading zero.
+ * You must not use any built-in BigInteger library or convert the inputs to
+ * integer directly.
+ * 
+ * 
+ */
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    
+};
