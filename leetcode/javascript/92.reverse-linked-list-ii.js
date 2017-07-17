8d245c2aa9b17f729120dcb4e4949f882e6c47e4
Hao Liu <haol@yahoo-inc.com>
2017-07-17 16:43:57 -0700

    WIP Q92

---
+++ leetcode/javascript/92.reverse-linked-list-ii.js
@@ -0,0 +1,45 @@
+/*
+ * [92] Reverse Linked List II
+ *
+ * https://leetcode.com/problems/reverse-linked-list-ii
+ *
+ * Medium (30.56%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '[5]\n1\n1'
+ *
+ * 
+ * Reverse a linked list from position m to n. Do it in-place and in
+ * one-pass.
+ * 
+ * 
+ * 
+ * For example:
+ * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+ * 
+ * 
+ * return 1->4->3->2->5->NULL.
+ * 
+ * 
+ * Note:
+ * Given m, n satisfy the following condition:
+ * 1 ? m ? n ? length of list.
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @param {number} m
+ * @param {number} n
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, m, n) {
+
+};
