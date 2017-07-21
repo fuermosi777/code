0f772a494b1359906c2b6aff0e2b15bed809f153
Hao Liu <haol@yahoo-inc.com>
2017-07-20 17:04:38 -0700

    WIP Q79

---
+++ leetcode/javascript/79.word-search.js
@@ -0,0 +1,44 @@
+/*
+ * [79] Word Search
+ *
+ * https://leetcode.com/problems/word-search
+ *
+ * algorithms
+ * Medium (26.55%)
+ * Total Accepted:    128.7K
+ * Total Submissions: 484.7K
+ * Testcase Example:  '["ABCE","SFCS","ADEE"]\n"ABCCED"'
+ *
+ * 
+ * Given a 2D board and a word, find if the word exists in the grid.
+ * 
+ * 
+ * The word can be constructed from letters of sequentially adjacent cell,
+ * where "adjacent" cells are those horizontally or vertically neighboring. The
+ * same letter cell may not be used more than once.
+ * 
+ * 
+ * 
+ * For example,
+ * Given board = 
+ * 
+ * [
+ * ⁠ ['A','B','C','E'],
+ * ⁠ ['S','F','C','S'],
+ * ⁠ ['A','D','E','E']
+ * ]
+ * 
+ * 
+ * word = "ABCCED", -> returns true,
+ * word = "SEE", -> returns true,
+ * word = "ABCB", -> returns false.
+ * 
+ */
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    
+};
