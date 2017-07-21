98fa0d82a9e931cc1b52159281311506475e1a49
Hao Liu <haol@yahoo-inc.com>
2017-07-21 14:20:01 -0700

    Q79

--- leetcode/javascript/79.word-search.js
+++ leetcode/javascript/79.word-search.js
@@ -34,11 +34,64 @@
  * word = "ABCB", -> returns false.
  * 
  */
+var Point = function(i, j) {
+  this.i = i;
+  this.j = j;
+}
+
+function adj(point, board, inword) {
+  let res = [];
+  let i = point.i, j = point.j;
+  if (i > 0 && !inword[`${i - 1}+${j}`]) {
+    res.push(new Point(i - 1, j));
+  }
+  if (j > 0 && !inword[`${i}+${j - 1}`]) {
+    res.push(new Point(i, j - 1));
+  }
+  if (i < board.length - 1 && !inword[`${i + 1}+${j}`]) {
+    res.push(new Point(i + 1, j));
+  }
+  if (j < board[0].length - 1 && !inword[`${i}+${j + 1}`]) {
+    res.push(new Point(i, j + 1));
+  }
+  return res;
+}
+
+function dfs(point, board, ct, word, inword) {
+  if (board[point.i][point.j] === word[ct]) {
+    ct++;
+    if (ct === word.length) return true;
+    inword[`${point.i}+${point.j}`] = true;
+    let adjs = adj(point, board, inword);
+    if (adjs.length === 0) return false;
+
+    for (let i = 0; i < adjs.length; i++) {
+      if (dfs(adjs[i], board, ct, word, inword)) {
+        return true;
+      }
+    }
+  } 
+
+  inword[`${point.i}+${point.j}`] = false;
+  return false;
+}
+
 /**
  * @param {character[][]} board
  * @param {string} word
  * @return {boolean}
  */
 var exist = function(board, word) {
-    
+  for (let m = 0; m < board.length; m++) {
+    for (let n = 0; n < board[m].length; n++) {
+      let inword = {};
+      let ct = 0;
+      let startpoint = new Point(m, n);
+      if (dfs(startpoint, board, ct, word, inword)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
