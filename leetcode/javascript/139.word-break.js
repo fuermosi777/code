54587dbd9ee5e928e2e31e012b3e7a6f000f3d15
Hao Liu <haol@yahoo-inc.com>
2017-07-21 17:02:06 -0700

    Q139

--- leetcode/javascript/139.word-break.js
+++ leetcode/javascript/139.word-break.js
@@ -1,21 +1,62 @@
-var map = {};
+/*
+ * [139] Word Break
+ *
+ * https://leetcode.com/problems/word-break
+ *
+ * algorithms
+ * Medium (29.65%)
+ * Total Accepted:    150.9K
+ * Total Submissions: 509K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * 
+ * Given a non-empty string s and a dictionary wordDict containing a list of
+ * non-empty words, determine if s can be segmented into a space-separated
+ * sequence of one or more dictionary words. You may assume the dictionary does
+ * not contain duplicate words.
+ * 
+ * 
+ * For example, given
+ * s = "leetcode",
+ * dict = ["leet", "code"].
+ * 
+ * 
+ * 
+ * Return true because "leetcode" can be segmented as "leet code".
+ * 
+ * 
+ * 
+ * UPDATE (2017/1/4):
+ * The wordDict parameter had been changed to a list of strings (instead of a
+ * set of strings). Please reload the code definition to get the latest
+ * changes.
+ * 
+ */
+
+function contains(s, dict, failed) {
+  if (dict.hasOwnProperty(s)) return true;
+  if (failed.hasOwnProperty(s)) return false;
+
+  for (let i = 1; i <= s.length; i++) {
+    if (dict.hasOwnProperty(s.substring(0, i))) {
+      if (contains(s.substring(i), dict, failed)) {
+        return true;
+      }
+    }
+  }
+  failed[s] = false;
+  return false;
+}
+
 /**
  * @param {string} s
  * @param {string[]} wordDict
  * @return {boolean}
  */
 var wordBreak = function(s, wordDict) {
-  if (s === '') return true;
-  if (map.hasOwnProperty(s)) return false;
-  for (let i = 0; i < wordDict.length; i++) {
-    let word = wordDict[i];
-
-    if (s.startsWith(word)) {
-      if (wordBreak(s.substring(word.length), wordDict)) {
-        return true;
-      }
-    }
-  }
-  map[s] = false;
-  return false;
+  let dict = {};
+  wordDict.forEach(word => dict[word] = true);
+  let failed = {};
+  
+  return contains(s, dict, failed);
 };
