393a4d00ca4bda61a482a06573db4bfb1914b6e4
fuermosi777 <liuhao1990@gmail.com>
2017-07-26 00:18:57 -0700

    WIP Q26

---
+++ leetcode/javascript/26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,37 @@
+/*
+ * [26] Remove Duplicates from Sorted Array
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array
+ *
+ * algorithms
+ * Easy (35.52%)
+ * Total Accepted:    241.2K
+ * Total Submissions: 679.3K
+ * Testcase Example:  '[]'
+ *
+ * 
+ * Given a sorted array, remove the duplicates in place such that each element
+ * appear only once and return the new length.
+ * 
+ * 
+ * Do not allocate extra space for another array, you must do this in place
+ * with constant memory.
+ * 
+ * 
+ * 
+ * For example,
+ * Given input array nums = [1,1,2],
+ * 
+ * 
+ * Your function should return length = 2, with the first two elements of nums
+ * being 1 and 2 respectively. It doesn't matter what you leave beyond the new
+ * length.
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+};
