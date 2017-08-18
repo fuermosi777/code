0441ab45243acb09317035581cdcb6badfe2a9a3
fuermosi777 <liuhao1990@gmail.com>
2017-08-17 23:12:31 -0700

    Q304

--- leetcode/javascript/304.range-sum-query-2d-immutable.js
+++ leetcode/javascript/304.range-sum-query-2d-immutable.js
@@ -49,6 +49,28 @@
 var NumMatrix = function(matrix) {
   this.map = {};
   this.matrix = matrix;
+
+  // init DP
+  this.dp = [];
+  for (let i = 0; i < this.matrix.length; i++) {
+    this.dp[i] = [];
+  }
+
+  for (let i = 0; i < this.matrix.length; i++) {
+    for (let j = 0; j < this.matrix[i].length; j++) {
+      let a = 0, b = 0, c = 0;
+      if (i - 1 >= 0) {
+        a = this.dp[i - 1][j];
+      }
+      if (j - 1 >= 0) {
+        b = this.dp[i][j - 1];
+      }
+      if (i - 1 >= 0 && j - 1 >= 0) {
+        c = this.dp[i - 1][j - 1];
+      }
+      this.dp[i][j] = this.matrix[i][j] + a + b - c;
+    }
+  }
 };
 
 /** 
@@ -59,18 +81,18 @@ var NumMatrix = function(matrix) {
  * @return {number}
  */
 NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
-  let lkey = `${row1},${col1}`;
-  if (this.map.hasOwnProperty(lkey)) {
-    let rKey = `${row2},${col2}`;
-    if (this.map[lkey].hasOwnProperty(rkey)) {
-      return this.map[lkey][rkey];
-    } else {
-      
-    }
-  } else {
-
+  let total = this.dp[row2][col2];
+  let a = 0, b = 0, c = 0;
+  if (row1 - 1 >= 0) {
+    a = this.dp[row1 - 1][col2];
   }
-
+  if (col1 - 1 >= 0) {
+    b = this.dp[row2][col1 - 1];
+  }
+  if (row1 - 1 >= 0 && col1 - 1 >= 0) {
+    c = this.dp[row1 - 1][col1 - 1];
+  }
+  return total - a - b + c;
 };
 
 /** 
