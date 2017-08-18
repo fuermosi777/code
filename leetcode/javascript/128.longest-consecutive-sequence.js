5a03ce8f7df4c8b22d39131e11d3c263927b9f27
Hao Liu <liuhao1990@gmail.com>
2017-08-18 14:44:10 -0700

    128

--- leetcode/javascript/128.longest-consecutive-sequence.js
+++ leetcode/javascript/128.longest-consecutive-sequence.js
@@ -24,22 +24,76 @@
  * 
  */
 /**
+ * O(nlogn) - edge cases: empty, duplicates
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var longestConsecutive = function(nums) {
+//   if (nums.length === 0) return 0;
+
+//   nums.sort((a, b) => (a - b));
+//   let max = 1, current = 1;
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] - nums[i - 1] === 1) {
+//       current++;
+//     } else if (nums[i] !== nums[i - 1]) {
+//       current = 1;
+//     }
+//     max = Math.max(max, current);
+//   }
+//   return max;
+// };
+
+/**
+ * O(n)
  * @param {number[]} nums
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-  nums.sort((a, b) => (a - b));
-  let max = 1, current = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] - nums[i - 1] === 1) {
-      current++;
+  if (nums.length === 0) return 0;
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let n = nums[i];
+    let left = n - 1, right = n + 1;
+    if (map[n]) continue;
+    if (map[left] && map[right]) {
+      if (map[left].start <= map[right].start && map[left].end >= map[right].end) continue;
+      if (map[right].start <= map[left].start && map[right].end >= map[left].end) continue;
+      let leftRange = map[left].start < map[right].start ? map[left] : map[right];
+      let rightRange = map[left] === leftRange ? map[right] : map[left];
+
+      let range = new Range(leftRange.start, rightRange.end);
+      delete map[leftRange.end];
+      delete map[rightRange.start];
+      map[leftRange.start] = range;
+      map[rightRange.end] = range;
+    } else if (map[left]) {
+      if (map[left].end >= n) continue;
+      let range = map[left];
+      let oldEnd = range.end;
+      if (oldEnd !== range.start) {
+        delete map[oldEnd];
+      }
+      range.end = n;
+      map[n] = range;
+    } else if (map[right]) {
+      if (map[right].start <= n) continue;
+      let range = map[right];
+      let oldStart = range.start;
+      if (oldStart !== range.end) {
+        delete map[oldStart];
+      }
+      range.start = n;
+      map[n] = range;
     } else {
-      max = Math.max(max, current);
-      current = 1;
-      i++;
+      map[n] = new Range(n, n);
     }
   }
-  return max;
+  // console.log(map)
+  return Math.max.apply(null, Object.keys(map).map(key => (map[key].end - map[key].start + 1)))
 };
 
-console.log(longestConsecutive([100, 4, 200, 1, 3, 2]))
+function Range(start, end) {
+  this.start = start;
+  this.end = end;
+}
