d14a0bcc9583ff10df748328972ad6f6c155c832
Hao Liu <liuhao1990@gmail.com>
2017-08-22 16:31:20 -0700

    523

---
+++ leetcode/javascript/523.continuous-subarray-sum.js
@@ -0,0 +1,91 @@
+/*
+ * [523] Continuous Subarray Sum
+ *
+ * https://leetcode.com/problems/continuous-subarray-sum
+ *
+ * algorithms
+ * Medium (22.69%)
+ * Total Accepted:    16.5K
+ * Total Submissions: 72.8K
+ * Testcase Example:  '[23,2,4,6,7]\n6'
+ *
+ * 
+ * Given a list of non-negative numbers and a target integer k, write a
+ * function to check if the array has a continuous subarray of size at least 2
+ * that sums up to the multiple of k, that is, sums up to n*k where n is also
+ * an integer.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: [23, 2, 4, 6, 7],  k=6
+ * Output: True
+ * Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up
+ * to 6.
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: [23, 2, 6, 4, 7],  k=6
+ * Output: True
+ * Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5
+ * and sums up to 42.
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The length of the array won't exceed 10,000.
+ * You may assume the sum of all the numbers is in the range of a signed 32-bit
+ * integer.
+ * 
+ * 
+ */
+
+Array.prototype.hasMoreThanOneZero = function() {
+  let last;
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === 0 && last === 0) return true;
+    last = this[i];
+  }
+  return false;
+};
+
+/**
+ * Too many traps: 1, 0, negative k...
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+  if (nums.length < 2) return false;
+  if (k < 0) k = -k;
+
+  if (nums.hasMoreThanOneZero() || k === 1) {
+    return true;
+  } 
+
+  let map = {0: -1};
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    let m = 1;
+    while (sum - k * m >= 0) {
+      if (map.hasOwnProperty(sum - k * m) && i - map[sum - k * m] >= 2) {
+        return true;
+      }
+      if (k !== 0 && k !== 1) {
+        m++;
+      } else {
+        break;
+      }
+    }
+    if (!map.hasOwnProperty(sum)) {
+      map[sum] = i;
+    }
+  }
+  return false;
+};
