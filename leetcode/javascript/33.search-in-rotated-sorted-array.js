945864443517a31909390191542d796412057a0f
Hao Liu <liuhao1990@gmail.com>
2017-09-14 17:05:34 -0700

    WIP Q33 Q81

--- leetcode/javascript/33.search-in-rotated-sorted-array.js
+++ leetcode/javascript/33.search-in-rotated-sorted-array.js
@@ -1,29 +1,52 @@
+/*
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array
+ *
+ * algorithms
+ * Medium (32.10%)
+ * Total Accepted:    192K
+ * Total Submissions: 598K
+ * Testcase Example:  '[]\n5'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * 
+ * You are given a target value to search. If found in the array return its
+ * index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ */
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 var search = function(nums, target) {
-  let r = 0;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] < nums[i - 1]) {
-      r = i;
+  let lo = 0, hi = nums.length - 1;
+  while (lo < hi) {
+    if (hi - lo === 1) { // only two vals
+      if (nums[hi] < nums[lo]) {
+        lo = hi;
+      }
+      break;
+    }
+    let mid = lo + (hi - lo) / 2 | 0;
+    if (nums[mid] < nums[lo]) {
+      hi = mid;
+    } else if (nums[mid] > nums[hi]) {
+      lo = mid;
+    } else {
       break;
     }
   }
-  let helper = nums.concat(nums);
-  let lo = r, hi = nums.length - 1 + r;
 
-  while (lo <= hi) {
-    let mid = lo + Math.floor((hi - lo) / 2);
-    if (helper[mid] === target) {
-      return mid % nums.length;
-    } else if (helper[mid] < target) {
-      lo = mid + 1;
-    } else {
-      hi = mid - 1;
-    }
+  hi = lo === 0 ? nums.length - 1 : lo - 1;
+  while (lo < hi) {
+    
   }
-
-  return -1;
 };
+
+console.log(search([7,0,1,2,3,4,5], 2));
---
+++ leetcode/javascript/81.search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,35 @@
+/*
+ * [81] Search in Rotated Sorted Array II
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii
+ *
+ * algorithms
+ * Medium (32.73%)
+ * Total Accepted:    100.9K
+ * Total Submissions: 308.4K
+ * Testcase Example:  '[]\n5'
+ *
+ * 
+ * Follow up for "Search in Rotated Sorted Array":
+ * What if duplicates are allowed?
+ * 
+ * Would this affect the run-time complexity? How and why?
+ * 
+ * 
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * 
+ * Write a function to determine if a given target is in the array.
+ * 
+ * The array may contain duplicates.
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+  
+};
