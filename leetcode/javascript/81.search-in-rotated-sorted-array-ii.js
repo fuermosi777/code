e38064d41715cd62af17952124323f457d42c027
Hao Liu <liuhao1990@gmail.com>
2017-09-15 12:56:48 -0700

    Q81

--- leetcode/javascript/81.search-in-rotated-sorted-array-ii.js
+++ leetcode/javascript/81.search-in-rotated-sorted-array-ii.js
@@ -25,11 +25,61 @@
  * 
  * The array may contain duplicates.
  */
+
+function searchStart(nums, lo, hi) {
+  if (hi <= lo) return lo;
+  if (hi - lo === 1) {
+    if (nums[hi] < nums[lo]) {
+      return hi;
+    } else {
+      return lo;
+    }
+  }
+
+  let mid = lo + (hi - lo) / 2 | 0;
+  if (nums[mid] > nums[lo]) {
+    return searchStart(nums, mid, hi);
+  } else if (nums[mid] < nums[hi]) {
+    return searchStart(nums, lo, mid);
+  } else {
+    let left = searchStart(nums, lo, mid);
+    let right = searchStart(nums, mid, hi);
+    if (left !== lo) return left;
+    if (right !== mid) return right;
+    return left;
+  }
+}
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {boolean}
  */
 var search = function(nums, target) {
+  if (nums.length === 0) return false;
+
+  let lo = 0, hi = nums.length - 1;
+  lo = searchStart(nums, lo, hi);
   
+  let res = false;
+  if (nums[0] <= target && target <= nums[lo - 1]) {
+    res = bs(0, lo - 1, nums, target) !== -1;
+  } else if (nums[lo] <= target && target <= nums[nums.length - 1]) {
+    res = bs(lo, nums.length - 1, nums, target) !== -1;
+  }
+
+  return res;
 };
+
+function bs(lo, hi, nums, target) {
+  while (lo < hi) {
+    let mid = lo + (hi - lo) / 2 | 0;
+    if (nums[mid] >= target) {
+      hi = mid;
+    } else {
+      lo = mid + 1;
+    }
+  }
+  if (nums[lo] === target) return lo;
+  return -1;
+}
