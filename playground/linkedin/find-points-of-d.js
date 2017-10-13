18ca9bb06b09dc168628ae02cd83c1a657815995
Hao Liu <liuhao1990@gmail.com>
2017-10-13 13:22:58 +0800

    points

---
+++ playground/linkedin/find-points-of-d.js
@@ -0,0 +1,19 @@
+/**
+ * For many points (object of {x: 1, y: 2}), find all the points which fall in r radius of a given point.
+ */
+
+/**
+ * @param  {object[]} points All points
+ * @param  {object} p The point
+ * @param  {number} r Radius
+ * @return {object[]}
+ */
+function find(points, p, r) {
+  // iterate all points...
+}
+
+/**
+ * Follow up: how to increase the performance?
+ *
+ * Divide the map into small pieces.
+ */
