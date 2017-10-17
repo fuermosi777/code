aba59819618dbc1ec33d1586894a68c4ab874bf5
Hao Liu <liuhao1990@gmail.com>
2017-10-17 15:35:43 -0700

    WIP Q lca

---
+++ playground/linkedin/lca-without-root.js
@@ -0,0 +1,14 @@
+/**
+ * Similar to LC 236, but no root.
+ */
+
+function TreeNode(parent, val) {
+  this.parent = parent;
+  this.val = val;
+  this.left = this.right = null;
+}
+
+var lowestCommonAncestor = function(p, q) {
+
+
+}
