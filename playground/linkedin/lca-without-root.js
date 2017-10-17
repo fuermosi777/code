4a5cdb4134a9b681ed4201e91078652d6da8056e
Hao Liu <liuhao1990@gmail.com>
2017-10-17 15:44:57 -0700

    Q lca

--- playground/linkedin/lca-without-root.js
+++ playground/linkedin/lca-without-root.js
@@ -8,7 +8,34 @@ function TreeNode(parent, val) {
   this.left = this.right = null;
 }
 
-var lowestCommonAncestor = function(p, q) {
+function lca(root, p, q) {
+  if (root === null) return null;
+  if (root === p || root === q) return root;
 
-
+  let left = lca(root.left, p, q);
+  let right = lca(root.right, p, q);
+  if (left && right) {
+    return root;
+  } else if (left) {
+    return left;
+  } else if (right) {
+    return right;
+  }
+  return null;
 }
+
+var lowestCommonAncestor = function(p, q) {
+  let root = p;
+  while (root.parent) {
+    root = root.parent;
+  }
+  return lca(root, p, q);
+}
+
+let a1 = new TreeNode(null, '1');
+let a2 = new TreeNode(a1, '2');
+let a3 = new TreeNode(a1, '3');
+a1.left = a2;
+a1.right = a3;
+
+console.log(lowestCommonAncestor(a2, a3).val);
