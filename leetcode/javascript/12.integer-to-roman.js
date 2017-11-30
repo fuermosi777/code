8032d857cd7822e85da0af58bc9c9a3c54300b29
Hao Liu <liuhao1990@gmail.com>
2017-11-30 10:22:01 -0800

    12

--- leetcode/javascript/12.integer-to-roman.js
+++ leetcode/javascript/12.integer-to-roman.js
@@ -22,10 +22,48 @@
  * D = 500
  * M = 1000
  */
+
+const Romans = [
+  null,
+  ['I', 'V', 'X'],
+  ['X', 'L', 'C'],
+  ['C', 'D', 'M'],
+  ['M']
+];
+
+function repeat(str, n) {
+  let res = '';
+  for (let i = 0; i < n; i++) {
+    res += str;
+  }
+  return res;
+}
+
+function convertDigitToRomain(num, digit) {
+  let n = Number(num);
+  if (n < 4) {
+    return repeat(Romans[digit][0], n);
+  } else if (n === 4) {
+    return Romans[digit][0] + Romans[digit][1];
+  } else if (n < 9) {
+    return Romans[digit][1] + repeat(Romans[digit][0], n - 5);
+  } else if (n === 9) {
+    return Romans[digit][0] + Romans[digit][2];
+  }
+
+  return ''; // num = '0'
+}
+
 /**
  * @param {number} num
  * @return {string}
  */
 var intToRoman = function(num) {
-  
+  let str = num.toString();
+  let res = '';
+  for (let i = str.length; i > 0; i--) {
+    res += convertDigitToRomain(str[str.length -  i], i);
+  }
+
+  return res;
 };
