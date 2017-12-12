976cee334a2dd7d967bade3f033fecbd25075378
fuermosi777 <liuhao1990@gmail.com>
2017-12-11 19:13:54 -0800

    querySelector

--- playground/google/getElementsByClassName-querySelector.js
+++ playground/google/getElementsByClassName-querySelector.js
@@ -19,3 +19,22 @@ function dfs(node, className, resList) {
     dfs(node.childNodes[i], className, resList);
   }
 }
+
+function querySelector(root, selector) {
+  let selectors = selector.split(',');
+  selectors.forEach(s => s = s.trim());
+
+  for (let i = 0; i < selectors.length; i++) {
+    let firstChar = selectors[i][0];
+    let className = '';
+    if (firstChar === '.') {
+      className = selectors[i].substring(1);
+
+      let nodes = getElementsByClassName(root, className);
+
+      if (nodes.length > 0) return nodes[0];
+    }
+  }
+
+  return null;
+}
