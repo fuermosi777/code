983b928d95a558bff8b544b216cda1e2252601f6
Hao Liu <liuhao1990@gmail.com>
2017-12-12 00:43:04 -0800

    event emitter

--- playground/google/event-emitter.js
+++ playground/google/event-emitter.js
@@ -1,13 +1,29 @@
 class EventEmitter {
   constructor() {
-
+    this.callbackMap = {};
   }
 
-  fire() {
-
+  fire(eventName) {
+    if (this.callbackMap.hasOwnProperty(eventName)) {
+      this.callbackMap[eventName].forEach(callback => {
+        callback();
+      })
+    } else {
+      return;
+    }
   }
 
   listenFor(eventName, callback) {
-    
+    if (!this.callbackMap.hasOwnProperty(eventName)) {
+      this.callbackMap[eventName] = [];
+    }
+    this.callbackMap[eventName].push(callback);
   }
 }
+
+let eventEmitter = new EventEmitter();
+
+eventEmitter.listenFor('testEvent', () => { console.log('testEvent is called') });
+eventEmitter.listenFor('testEvent', () => { console.log('another testEvent is called') });
+
+eventEmitter.fire('testEvent');
