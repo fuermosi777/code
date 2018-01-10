547aedf1894eb1238f1c4c39bad01e4c591a5422
Hao Liu <liuhao1990@gmail.com>
2018-01-09 17:52:57 -0800

    WIP thread bt

--- playground/thread-binary-tree.js
+++ playground/thread-binary-tree.js
@@ -1,36 +1,88 @@
 var { TreeNode, buildTreeFromArray } = require('../leetcode/javascript/util/TreeNode');
 
-function addThread(binaryTreeNode) {
-  let inorder = [];
-  dfs(binaryTreeNode, inorder);
+// function addThread(binaryTreeNode) {
+//   let inorder = [];
+//   dfs(binaryTreeNode, inorder);
 
-  for (let i = 0; i < inorder.length; i++) {
-    let prev, next, node = inorder[i];
-    if (i > 0) {
-      prev = inorder[i - 1];
-    }
-    if (i < inorder.length - 1) {
-      next = inorder[i + 1];
-    }
-    if (prev && !node.left) {
-      node.left = prev;
-    }
-    if (next && !node.right) {
-      node.right = next;
-    }
+//   for (let i = 0; i < inorder.length; i++) {
+//     let prev, next, node = inorder[i];
+//     if (i > 0) {
+//       prev = inorder[i - 1];
+//     }
+//     if (i < inorder.length - 1) {
+//       next = inorder[i + 1];
+//     }
+//     if (prev && !node.left) {
+//       node.left = prev;
+//     }
+//     if (next && !node.right) {
+//       node.right = next;
+//     }
+//   }
+// }
+
+// function dfs(node, seq) {
+//   if (!node) return;
+
+//   dfs(node.left, seq);
+//   seq.push(node);
+//   dfs(node.right, seq);
+// }
+
+// ===
+
+function addThreadInPlace(binaryTreeNode) {
+  // addParent(binaryTreeNode);
+
+  dfs(binaryTreeNode);
+}
+
+// function addParent(binaryTreeNode) {
+//   _addP(binaryTreeNode);
+// }
+
+// function _addP(node) {
+//   if (!node) return;
+
+//   if (node.left) node.left.parent = node;
+//   if (node.right) node.right.parent = node;
+//   _addP(node.left);
+//   _addP(node.right);
+// }
+
+function dfs(node, prev) {
+  if (!node) return null;
+
+  dfs(node.left, prev);
+  
+  if (!node.left) {
+    node.left = prev;
+  }
+  prev = node;
+
+  dfs(node.right, prev);
+
+
+  // return node;
+}
+
+let tree = buildTreeFromArray([1, 2, 3, 4, 5]);
+
+// addThread(tree);
+
+
+
+
+addThreadInPlace(tree);
+
+function printThreadTree(tree) {
+  let node = tree;
+  while (node) {
+    console.log(node.val);
+    node = node.right
   }
 }
 
-function dfs(node, seq) {
-  if (!node) return;
+// console.log(tree.left)
 
-  dfs(node.left, seq);
-  seq.push(node);
-  dfs(node.right, seq);
-}
-
-let tree = buildTreeFromArray([1, 2, 3, 4, 5, 6]);
-
-addThread(tree);
-
-console.dir(tree);
+printThreadTree(tree.left.left)
