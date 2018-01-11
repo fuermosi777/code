1290ea0b50cda05debb58ca38342aa1bc226d65b
fuermosi777 <liuhao1990@gmail.com>
2018-01-10 22:46:44 -0800

    wip 322

---
+++ leetcode/javascript/322.coin-change.js
@@ -0,0 +1,46 @@
+/*
+ * [322] Coin Change
+ *
+ * https://leetcode.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (26.74%)
+ * Total Accepted:    80.9K
+ * Total Submissions: 302.7K
+ * Testcase Example:  '[1]\n0'
+ *
+ * 
+ * You are given coins of different denominations and a total amount of money
+ * amount. Write a function to compute the fewest number of coins that you need
+ * to make up that amount. If that amount of money cannot be made up by any
+ * combination of the coins, return -1.
+ * 
+ * 
+ * 
+ * Example 1:
+ * coins = [1, 2, 5], amount = 11
+ * return 3 (11 = 5 + 5 + 1)
+ * 
+ * 
+ * 
+ * Example 2:
+ * coins = [2], amount = 3
+ * return -1.
+ * 
+ * 
+ * 
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ * 
+ * 
+ * Credits:Special thanks to @jianchao.li.fighter for adding this problem and
+ * creating all test cases.
+ */
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    
+};
