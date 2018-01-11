cba505f582faa35a499d674a7bfbb047608870ec
Hao Liu <liuhao1990@gmail.com>
2018-01-10 16:50:06 -0800

    wip 148 725

---
+++ leetcode/javascript/148.sort-list.js
@@ -0,0 +1,27 @@
+/*
+ * [148] Sort List
+ *
+ * https://leetcode.com/problems/sort-list/description/
+ *
+ * algorithms
+ * Medium (29.44%)
+ * Total Accepted:    122.3K
+ * Total Submissions: 415.4K
+ * Testcase Example:  '[]'
+ *
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    
+};
