60285f786f3dd83f6f9bc493f9adc7f65ad7c148
fuermosi777 <liuhao1990@gmail.com>
2018-01-10 22:10:16 -0800

    148

--- leetcode/javascript/148.sort-list.js
+++ leetcode/javascript/148.sort-list.js
@@ -23,5 +23,52 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
-    
+  if (!head) return null;
+
+  let slow = head, fast = head, prev;
+
+  while (fast.next && fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  if (!head.next) {
+    return head;
+  }
+
+  let head2 = slow.next;
+  slow.next = null;
+
+  let l1 = sortList(head);
+  let l2 = sortList(head2);
+
+  return merge(l1, l2);
 };
+
+function merge(n1, n2) {
+  let head = new ListNode();
+  let cur = head;
+  let node1 = n1;
+  let node2 = n2;
+  while (node1 || node2) {
+    if (node1 && node2) {
+      if (node1.val > node2.val) {
+        cur.next = node2;
+        node2 = node2.next;
+      } else {
+        cur.next = node1;
+        node1 = node1.next;
+      }
+    } else if (node1) {
+      cur.next = node1;
+      node1 = node1.next;
+    } else {
+      cur.next = node2;
+      node2 = node2.next;
+    }
+    cur = cur.next;
+  }
+
+  return head.next;
+}
+
