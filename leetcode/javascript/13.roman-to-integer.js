487f16a70e1c65f26773026104f2f599b6052865
fuermosi777 <liuhao1990@gmail.com>
2018-01-16 19:49:37 -0800

    13

--- leetcode/javascript/13.roman-to-integer.js
+++ leetcode/javascript/13.roman-to-integer.js
@@ -18,5 +18,25 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-  
+  let map = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  };
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+    let ch = s[s.length - 1 - i];
+    let lastCh = i > 0 ? s[s.length - i] : null;
+    if (lastCh && map[ch] < map[lastCh]) {
+      res -= map[ch];
+    } else {
+      res += map[ch]
+    }
+  }
+
+  return res;
 };
