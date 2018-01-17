81b9036dbc9a180ca217b24fb8a6e65882ae0824
Hao Liu <liuhao1990@gmail.com>
2018-01-16 16:08:12 -0800

    wip 12

---
+++ leetcode/javascript/12.integer-to-roman.0.js
@@ -0,0 +1,23 @@
+/*
+ * [12] Integer to Roman
+ *
+ * https://leetcode.com/problems/integer-to-roman/description/
+ *
+ * algorithms
+ * Medium (45.77%)
+ * Total Accepted:    128.9K
+ * Total Submissions: 281.5K
+ * Testcase Example:  '1'
+ *
+ * Given an integer, convert it to a roman numeral.
+ * 
+ * 
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    
+};
