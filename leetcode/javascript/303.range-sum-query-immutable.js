22ca77d5a57c5c22e9e856a933cd3462318459be
fuermosi777 <liuhao1990@gmail.com>
2018-01-17 23:07:53 -0800

    303

---
+++ leetcode/javascript/303.range-sum-query-immutable.js
@@ -0,0 +1,56 @@
+/*
+ * [303] Range Sum Query - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (31.41%)
+ * Total Accepted:    86.7K
+ * Total Submissions: 276K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * Given an integer array nums, find the sum of the elements between indices i
+ * and j (i ≤ j), inclusive.
+ * 
+ * Example:
+ * 
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+  this.sum = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    this.sum[i] = this.sum[i - 1] + nums[i];
+  }
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+  let start = i === 0 ? 0 : this.sum[i - 1];
+  return this.sum[j] - start;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = Object.create(NumArray).createNew(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
